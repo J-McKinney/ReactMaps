eefae2387db9a3237153737a3de15155023d06fc
Jesse <jessemckinney513@gmail.com>
2021-09-26 19:33:11 -0400

    yay

--- src/Components/Maps/maps.js
+++ src/Components/Maps/maps.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import Wrapper from "../Wrapper/wrapper";
 import Styles from "./maps.module.css";
 
 class maps extends Component {
   render() {
     return (
       <>
-        <Wrapper>
-          <div className={Styles.wrapper}></div>
-        </Wrapper>
+        <div className={Styles.wrapper}></div>
       </>
     );
   }
