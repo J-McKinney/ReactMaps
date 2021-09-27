8a44c2790d15716c1fa0fd11d1a6a73340b9d7a7
Jesse <jessemckinney513@gmail.com>
2021-09-27 08:23:16 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-// import logo from "./logo.svg";
-import Maps from "./Components/Maps/maps";
+// // import logo from "./logo.svg";
+// import Maps from "./Components/Maps/maps";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <Maps />
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+// import { render } from "react-dom";
+import Map from "./Components/Maps/maps";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    <Maps />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Map
+        id="myMap"
+        options={{
+          center: { lat: -28.024, lng: 140.887 },
+          zoom: 3,
+        }}
+      />
+    );
+  }
 }
 
 export default App;
